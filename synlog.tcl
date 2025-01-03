9437a866a04b4b3369c0bf566fd5dccc92013c2b
Michał Karaś <m.karas98@gmail.com>
2025-01-03 14:14:35 +0100

    Initial commit

---
+++ synlog.tcl
@@ -0,0 +1,49 @@
+history clear
+project -load ICE40UP5K_PROGRAM_syn.prj
+text_select 16 13 16 14
+text_select 17 9 17 15
+text_select 17 9 17 53
+text_select 18 9 18 12
+text_select 18 10 18 12
+text_select 19 10 19 12
+text_select 19 10 19 12
+text_select 16 9 16 12
+text_select 104 13 104 17
+text_select 104 13 104 24
+text_select 115 13 115 19
+text_select 124 22 124 24
+text_select 16 9 19 57
+text_select 16 1 19 57
+text_select 267 2 268 57
+text_select 270 29 270 61
+text_select 269 29 269 61
+text_select 268 29 268 61
+text_select 267 29 267 62
+text_select 55 9 56 45
+text_select 55 17 55 18
+text_select 141 13 142 35
+text_select 271 16 271 36
+text_select 273 16 273 17
+text_select 272 33 272 35
+text_select 273 33 273 35
+text_select 274 33 274 35
+text_select 271 1 274 38
+text_select 314 1 315 57
+text_select 314 32 314 35
+text_select 315 32 315 35
+text_select 316 32 316 35
+text_select 317 32 317 35
+project -run  
+project -run  
+text_select 76 13 77 27
+text_select 93 21 93 32
+text_select 77 14 77 16
+text_select 78 14 78 16
+text_select 79 14 79 16
+text_select 75 22 76 22
+text_select 76 21 76 22
+text_select 77 21 77 22
+text_select 78 21 78 22
+text_select 79 21 79 22
+project -run  
+project -close "C:/Users/Michal/Documents/Magisterka/Magisterka dokumenty wyjsciowe/FPGA/PROGRAM/CONTROL_FPGA_QSW_DC_DC_10KW_FPGA_VHDL-main/ICE40UP5K_PROGRAM_syn.prj"
