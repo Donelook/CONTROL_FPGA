82d6cad827d53c6a9c0ad7d6ca4dbe8006106f16
Michał Karaś <m.karas98@gmail.com>
2025-03-19 00:13:38 +0100

    PWM GENERATOR WORKS
    Working PWM generator

--- phase_controller.vhd
+++ phase_controller.vhd
@@ -114,9 +114,9 @@ begin
                     S2 <= '0';
 				--start_timer_hc <= '0';  -- Stop the stoper for delay_hc after it starts
                     if hc_time_passed = '1' then
-						start_timer_hc <= '0';  -- Stop the stoper for delay_hc after it starts
+			start_timer_hc <= '0';  -- Stop the stoper for delay_hc after it starts
                         state <= T23A_STATE;
-                        start_timer_tr <= '1';  -- Start the stoper for delay_tr
+                       
                     end if;
 
                 when T23A_STATE =>
@@ -135,7 +135,7 @@ begin
                     S1 <= '0';
                     S2 <= '0';
                     if tr_time_passed = '1' then
-						start_timer_tr <= '0';  -- Stop the stoper for delay_tr
+			start_timer_tr <= '0';  -- Stop the stoper for delay_tr
 
                         state <= T01A_STATE;
                     end if;
--- synlog.tcl
+++ synlog.tcl
@@ -1,4 +1,5 @@
 history clear
 project -load ICE40UP5K_PROGRAM_syn.prj
-text_select 192 85 192 86
+text_select 34 9 35 1
+project -run  
 project -close "C:/Users/Michal/Documents/Magisterka/Magisterka dokumenty wyjsciowe/FPGA/PROGRAM/CONTROL_FPGA_QSW_DC_DC_10KW_FPGA_VHDL-main/ICE40UP5K_PROGRAM_syn.prj"
