099e6077bf7239430028301a997bfb2db1146e76
Michał Karaś <m.karas98@gmail.com>
2025-10-16 21:10:13 +0200

    new delay

--- synlog.tcl
+++ synlog.tcl
@@ -1,1 +1,56 @@
+history clear
 project -load ICE40UP5K_PROGRAM_syn.prj
+text_select 133 25 133 39
+text_select 62 26 62 40
+text_select 30 9 30 19
+text_select 79 13 79 24
+text_select 171 17 176 24
+text_select 108 33 108 35
+text_select 108 67 108 69
+text_select 109 27 109 29
+text_select 109 50 109 52
+text_select 110 36 110 38
+text_select 111 27 111 29
+text_select 165 9 211 16
+text_select 94 9 157 16
+text_select 97 22 97 24
+text_select 98 29 98 31
+text_select 99 28 99 30
+text_select 100 23 100 25
+text_select 103 22 103 24
+text_select 103 38 103 40
+text_select 106 33 106 35
+text_select 106 67 106 69
+text_select 107 27 107 29
+text_select 107 50 107 52
+text_select 108 37 108 38
+text_select 108 36 108 38
+text_select 109 27 109 29
+text_select 112 22 112 24
+text_select 115 34 115 36
+text_select 115 59 115 61
+text_select 116 41 116 43
+text_select 117 40 117 42
+text_select 118 29 118 31
+text_select 120 41 120 43
+text_select 121 40 121 42
+text_select 126 34 126 36
+text_select 126 59 126 61
+text_select 127 41 127 43
+text_select 128 40 128 42
+text_select 129 29 129 31
+text_select 131 41 131 43
+text_select 132 40 132 42
+text_select 133 29 133 31
+text_select 137 25 137 27
+project -run  
+text_select 63 1 66 1
+project -run  
+project -run  
+text_select 1 1 203 16
+text_select 96 17 96 41
+project -run  
+text_select 122 29 122 32
+text_select 122 29 122 31
+project -run  
+project -close "C:/Users/Michal/Documents/Magisterka/Magisterka dokumenty wyjsciowe/FPGA/PROGRAM/CONTROL_FPGA_QSW_DC_DC_10KW_FPGA_VHDL-main/ICE40UP5K_PROGRAM_syn.prj"
